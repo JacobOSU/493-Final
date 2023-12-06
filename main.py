0f83364259a0bce93307e2a9e44e7b496e0f52ae
JacobOSU <97179651+JacobOSU@users.noreply.github.com>
2023-12-06 08:59:32 -0600

    restored files

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+import boat
+import load
+
+app = Flask(__name__)
+app.register_blueprint(boat.bp)
+app.register_blueprint(load.bp)
+
+@app.route('/')
+def index():
+    return "Please navigate to /lodgings to use this API"
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
